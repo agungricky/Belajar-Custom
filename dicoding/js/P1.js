4e864ad8c0947796744835136bca3364b0b7325d
agungricky07 <agungricky07@gmail.com>
2022-09-08 04:31:50 +0700

    Update Folder pembelajaran

---
+++ dicoding/js/P1.js
@@ -0,0 +1,35 @@
+// console.log("Hallo Sayang");
+// alert("Hallo");
+// document.write("Bunda Ika");
+
+// var tabel='Kotak 1';
+// var tabel=2;
+// var ibuk;
+// console.log(ibuk);
+
+// var x = 2;
+// var y = 4;
+// var c= y ** 2;
+// document.write(c);
+
+// var ku=5;
+// var coba=3;
+// document.write(ku+=1);
+
+// BigNumber nilai=100,000*9,000,000n;
+// document.write(nilai);
+
+// document.write("Apakah yakin ingi keluar?");
+// var i= alert();;
+// while(i=true){
+//     document.write("Berhasil");
+//     i++;
+// }
+
+
+
+if ((true || false) && (false || false)) {
+    console.log("It's true");
+} else {
+    console.log("It's false");
+}
